1c4a76a9781893267feeafd9b4f99a3253b631cb
JorgeAcevedo <jorge.acevedoj@hotmail.com>
2017-10-30 06:11:36 -0600

    Adding functions to the layer HAL

--- Software/WLProjectA_J/include/HAL/InitLPIT.h
+++ Software/WLProjectA_J/include/HAL/InitLPIT.h
@@ -4,18 +4,16 @@
 /*                        OBJECT SPECIFICATION                                */
 /*============================================================================*/
 /*!
- * $Source: WriteReadPins.h $
+ * $Source: InitLPIT.h $
  * $Revision: 1 $
  * $Author: Jorge Acevedo $
- * $Date: 24/10/2017 $
+ * $Date: 26/10/2017 $
  */
 /*============================================================================*/
 /* DESCRIPTION :                                                              */
-/** \file
-    function prototyping for:
-    Turning on the LED in bar
-    Turning off the green and blue LED
-    Reading the buttons
+/** \InitLPIT
+    functions to initializate interrupt timer.
+
 */
 /*============================================================================*/
 /* COPYRIGHT (C) CONTINENTAL AUTOMOTIVE 2014                                  */
@@ -32,78 +30,35 @@
 /*============================================================================*/
 /*                    REUSE HISTORY - taken over from                         */
 /*============================================================================*/
-/*  AUTHOR           |       VERSION      |          DESCRIPTION              */
+/*  AUTHOR             |        VERSION     |  DESCRIPTION                    */
 /*----------------------------------------------------------------------------*/
-/* Jorge Acevedo      |         1          |  FUNCTION PROTOTYPING WRITING    */
-/*                     |                    | ON LED AND READING BUTTON       */
-/*----------------------------------------------------------------------------*/
-/*                     |                    |                                 */
+/*Jorge Acevedo        |         1          | LPIT initialization functions   */
 /*============================================================================*/
 /*                               OBJECT HISTORY                               */
 /*============================================================================*/
 /*
  * $Log: filename.h  $
   ============================================================================*/
-#ifndef WRITEREADPINS_H_
-#define WRITEREADPINS_H_
-
-
+#ifndef INITLPIT_H
+#define INITLPIT_H
 
 /* Includes */
 /*============================================================================*/
-#ifndef MAL_MAPING_TYPEDEF_H_
-#include "MCAL\Maping_TypeDef.h"
-#endif
-
-#define LED10 (T_ULONG)10
-#define LED9  (T_ULONG)9
-#define LED8  (T_ULONG)8
-#define LED7  (T_ULONG)7
-#define LED6  (T_ULONG)6
-#define LED5  (T_ULONG)5
-#define LED4  (T_ULONG)4
-#define LED3  (T_ULONG)3
-#define LED2  (T_ULONG)2
-#define LED1  (T_ULONG)1
-
-#define BLUE_LED  (T_ULONG)11
-#define GREEN_LED (T_ULONG)12
-
-#define ButtonUp    (T_ULONG)13
-#define ButtonDw    (T_ULONG)14
-#define ButtonPinch (T_ULONG)15
-
-
+#include "MAL/LPIT.h"
+#include "HAL/InitClock.h"
 
 /* Constants and types */
 /*============================================================================*/
-
-
+#define LPIT0 (T_UBYTE)0
+#define LPIT1 (T_UBYTE)1
+#define LPIT2 (T_UBYTE)2
+#define LPIT3 (T_UBYTE)3
 /* Exported Variables */
 /*============================================================================*/
 
 
 /* Exported functions prototypes */
-void TurnOnLed (T_ULONG);
-void TurnOffLed(T_ULONG);
-T_ULONG ReadButton(T_ULONG);
 /*============================================================================*/
+void EnableLPIT (T_UBYTE Channel, T_UBYTE Timer);
 
 #endif  /* Notice: the file ends with a blank new line to avoid compiler warnings */
-
-
-/*
-
-
-* WriteRead_Pins.h
- *
- *  Created on: 24 oct. 2017
- *      Author: Jorge Acevedo
- */
-
-#ifndef HAL_WRITEREAD_PINS_H_
-#define HAL_WRITEREAD_PINS_H_
-
-
-
-#endif /* HAL_WRITEREAD_PINS_H_ */
